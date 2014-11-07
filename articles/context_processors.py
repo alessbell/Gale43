7098ae70c5c3b3c358ea4942f2372a0840dccb2d
Alessia Bellisario <bellisario.alessia@gmail.com>
2014-11-07 09:52:37 -0500

    Assignment completed, just changing out the logos

--- articles/context_processors.py
+++ articles/context_processors.py
@@ -1,7 +1,3 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-from django.core.urlresolvers import reverse
-from django.http import Http404
 from articles.models import Article
 
 def articles_processor(request):
--- articles/urls.py
+++ articles/urls.py
@@ -5,5 +5,4 @@ from articles import views
 urlpatterns = patterns('',
   url(r'^$', views.index, name='index'),
   url(r'^(?P<article_id>\d+)/$', views.detail, name='detail'),
-  url(r'^(?P<article_id>\d+)/show_articles/$', views.show_articles, name='show_articles'),
 )
